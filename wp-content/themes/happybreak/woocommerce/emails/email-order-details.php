fe6b2c2a1f1e3ba7e38335190173029c37f8a41d
Mohamed KRISTOU <krisstwo@gmail.com>
2017-10-09 15:26:51 +0100

    Order email fixes (labels, spacing)

--- wp-content/themes/happybreak/woocommerce/emails/email-order-details.php
+++ wp-content/themes/happybreak/woocommerce/emails/email-order-details.php
@@ -26,17 +26,17 @@ do_action( 'woocommerce_email_before_order_table', $order, $sent_to_admin, $plai
 
 <h3 style="color: #000000; font-weight: 800"><?php _e( 'VOTRE COMMANDE', 'woocommerce' ); ?></h3>
 <p>
-    <?php _e( 'N° de commande : ', 'woocommerce' ); ?> : <?php echo $order->get_order_number() ?>
-</p>
-<p>
-    <?php _e( 'Date de la commande  : ', 'woocommerce' ); ?> : <?php
+    <?php _e( 'N° de commande', 'happybreak' ); ?> : <?php echo $order->get_order_number() ?>
+<br/>
+    <?php _e( 'Date de la commande', 'happybreak' ); ?> : <?php
     $cls_date = new DateTime($order->get_date_created());
     echo $cls_date->format('d/m/Y h:i');
      ?>
 </p>
 
 <p>
-    <?php _e( 'Produits dans la commande  : ', 'woocommerce' ); ?>
+    <?php $totals = $order->get_order_item_totals(); ?>
+    <?php _e( 'Produits dans la commande', 'happybreak' ); ?> :
 </p>
 
 <p>
@@ -49,19 +49,10 @@ do_action( 'woocommerce_email_before_order_table', $order, $sent_to_admin, $plai
     ) ); ?>
 </p>
 <p>
-    <?php _e( 'Total TTC de la commande  :  ', 'woocommerce' ); ?>
+    <?php _e( 'Total TTC de la commande', 'happybreak' ); ?> : <?= wc_price($order->get_total()); ?>
+    <br/>
+    <?= $totals['payment_method']['label']; ?> <?= $totals['payment_method']['value']; ?>
 </p>
-		<?php
-			if ( $totals = $order->get_order_item_totals() ) {
-				$i = 0;
-				foreach ( $totals as $total ) {
-					$i++;
-					?><?php echo $total['label']; ?>
-						<?php echo $total['value']; ?>
-					<?php
-				}
-			}
-		?>
 
 
 <?php do_action( 'woocommerce_email_after_order_table', $order, $sent_to_admin, $plain_text, $email ); ?>
