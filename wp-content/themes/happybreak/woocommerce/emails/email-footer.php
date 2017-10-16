5de208ac7b35ba66e5de5b45bde4f44cca0a87b1
Mohamed KRISTOU <krisstwo@gmail.com>
2017-10-16 17:29:06 +0100

    Order complete email changes

--- wp-content/themes/happybreak/woocommerce/emails/email-footer.php
+++ wp-content/themes/happybreak/woocommerce/emails/email-footer.php
@@ -1,10 +1,8 @@
-<br>
-<h3 ><?php _e( 'COMMENT CA MARCHE?', 'woocommerce' ); ?></h3>
-<p><?php _e( 'Pour activer votre numéro, rien de plus simple :', 'woocommerce' ); ?></p>
-<p><?php _e( 'Connectez-vous à votre compte avec votre login et mot de passe habituel', 'woocommerce' ); ?></p>
-<p><?php _e( "Puis saisissez votre numéro de carte provisoire dans le champ « Saisissez votre numéro de membre ». Après l'avoir « validé », vous pourrez accéder à l’ensemble des informations permettant de contacter nos hôteliers partenaires.", 'woocommerce' ); ?></p>
-<p style="font-weight: 800"><?php _e( "Rappel : la durée de validité de la carte ne s'enclenche que le jour de l’activation.
-", 'woocommerce' ); ?></p>
-<p><?php _e( "Merci pour votre commande sur notre site,", 'woocommerce' ); ?></p>
-<p><?php _e( "Merci de ne pas répondre à cet email. Pour nous contacter, rendez-vous sur le site happybreak.com et cliquez sur « contactez-nous » ou, appelez-nous au 09 80 01 01 01 (de 8h à 22h, 7j/7, appel non surtaxé).", 'woocommerce' ); ?></p>
-<p><?php _e( "A bientôt sur happybreak.com.", 'woocommerce' ); ?></p>
+<br/>
+<br/>
+<p><?php _e( "Merci de ne pas répondre à cet email. Pour nous contacter, rendez-vous sur le site happybreak.com et cliquez sur « contactez-nous » ou, appelez-nous au 09 80 01 01 01 (de 8h à 22h, 7j/7, appel non surtaxé).", 'happybreak' ); ?></p>
+<p><?php _e( "A bientôt sur happybreak.com.", 'happybreak' ); ?></p>
+<p><?php _e( "L'équipe Happybreak", 'happybreak' ); ?></p>
+<br/>
+<p style="text-align: center;"><?php _e( "contact@happybreak.com - 09 80 01 01 01 - www.happybreak.com", 'happybreak' ); ?></p>
+<p style="text-align: center;"><?php _e( "Happybreak est une marque de la société BLOOM, SAS au capital de 412 500€ - 803 602 226 RCS Lyon – NAF 4791B - TVA intracommunautaire : FR21803602226", 'happybreak' ); ?></p>
--- wp-content/themes/happybreak/woocommerce/emails/email-header.php
+++ wp-content/themes/happybreak/woocommerce/emails/email-header.php
@@ -32,25 +32,16 @@ if ( ! defined( 'ABSPATH' ) ) {
 			<table border="0" cellpadding="0" cellspacing="0" height="100%" width="100%">
 				<tr>
 					<td align="center" valign="top">
-						<div id="template_header_image">
-							<?php
-								if ( $img = get_option( 'woocommerce_email_header_image' ) ) {
-									echo '<p style="margin-top:0;"><img src="' . esc_url( $img ) . '" alt="' . get_bloginfo( 'name', 'display' ) . '" /></p>';
-								}
-							?>
-						</div>
 						<table border="0" cellpadding="0" cellspacing="0" width="600" id="template_container">
 							<tr>
 								<td align="center" valign="top">
-									<!-- Header -->
-									<table border="0" cellpadding="0" cellspacing="0" width="600" id="template_header">
-										<tr>
-											<td id="header_wrapper">
-												<h1><?php echo $email_heading; ?></h1>
-											</td>
-										</tr>
-									</table>
-									<!-- End Header -->
+                                    <div id="template_header_image">
+                                        <?php
+                                        if ( $img = get_option( 'woocommerce_email_header_image' ) ) {
+                                            echo '<p style="margin-top:0;"><img src="' . esc_url( $img ) . '" alt="' . get_bloginfo( 'name', 'display' ) . '" /></p>';
+                                        }
+                                        ?>
+                                    </div>
 								</td>
 							</tr>
 							<tr>
