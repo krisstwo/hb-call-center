73827f2d81cbd9dcaa6b7c2af6e48e4f562f740d
Mohamed KRISTOU <krisstwo@gmail.com>
2017-10-16 17:38:26 +0100

    Invoie email changes

--- wp-content/themes/happybreak/woocommerce/emails/customer-invoice.php
+++ wp-content/themes/happybreak/woocommerce/emails/customer-invoice.php
@@ -11,29 +11,27 @@ if ( ! defined( 'ABSPATH' ) ) {
 do_action( 'woocommerce_email_header', $email_heading, $email ); ?>
 
 
-    <p><?php _e( ' Bonjour ', 'woocommerce' ); ?> <?php echo $order->get_billing_first_name() . ' ' .$order->get_billing_last_name()  ?></p>
+    <p><?php _e( ' Bonjour ', 'happybreak' ); ?> <?php echo $order->get_billing_first_name() . ' ' .$order->get_billing_last_name()  ?></p>
 
-    <p> <?php _e( 'Nous avons bien pris en compte votre pré-commande passée le ', 'woocommerce' ); ?>
+    <p> <?php _e( 'Nous avons bien pris en compte votre précommande passée le', 'happybreak' ); ?>
         <?php
         $cls_date = new DateTime($order->get_date_created());
         echo $cls_date->format('d/m/Y -  h:i'); ?>
-        <?php _e( 'et nous vous en remercions.', 'woocommerce' ); ?>
+        <?php _e( 'et nous vous en remercions.', 'happybreak' ); ?>
 
 
     </p>
   <?php if ( $order->has_status( 'pending' ) ) : ?>
-    <p><?php printf( __( 'Cliquez sur ce lien, découvrez la carte Happybreak et commandez en ligne: %2$s', 'woocommerce' ), get_bloginfo( 'name', 'display' ), '<a href="' . esc_url( $order->get_checkout_payment_url() ) . '">' . __( 'pay', 'woocommerce' ) . '</a>' ); ?></p>
+    <p><?php printf( __( 'Cliquez sur ce lien, et commandez en ligne: %2$s', 'happybreak' ), get_bloginfo( 'name', 'display' ), '<a href="' . esc_url( $order->get_checkout_payment_url() ) . '">' . __( 'payer', 'happybreak' ) . '</a>' ); ?></p>
 <?php endif; ?>
 
 
-    <p><?php _e( 'Nous vous remercions et vous souhaitons de très beaux séjours', 'woocommerce' ); ?>
+    <p><?php _e( 'Nous vous remercions et vous souhaitons de très beaux séjours', 'happybreak' ); ?>
         </p>
-
-    <p><?php _e( ' L’équipe Happybreak
-        Merci de ne pas répondre à cet email. Pour nous contacter, rendez-vous sur le site <a href="www.happybreak.com">happybreak.com</a> et cliquez sur « contactez-nous » ou, appelez-nous au 09 80 01 01 01 (de 8h à 22h, 7j/7, appel non surtaxé).', 'woocommerce' ); ?>
-    </p>
-    <p><?php _e( ' A bientôt sur <a href="www.happybreak.com"> happybreak.com</a>', 'woocommerce' ); ?>
-    </p>
-
 <?php
 
+/**
+ * @hooked WC_Emails::email_footer() Output the email footer
+ */
+do_action( 'woocommerce_email_footer', $email );
+?>
